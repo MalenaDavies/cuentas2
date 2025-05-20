74d17ad745134f495fe1f219095e411d7a3cac48
ernestina145 <ernestina.zvicer@hospitalitaliano.org.ar>
2025-05-20 15:03:59 -0300

    ejercicio3

---
+++ operacionesdetres/operacionesdetres/operacionesdetres.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b, c, suma, promedio, producto, mayor, menor;
+
+    cout << "Ingrese el primer numero que desea calcular: ";
+    cin >> a;
+
+    cout << "Ingrese el segundo numero que desea calcular: ";
+    cin >> b;
+
+    cout << "Ingrese el tercer numero que desea calcular: ";
+    cin >> c;
+
+    suma = a + b + c;
+    producto = a * b * c;
+    promedio = (a + b + c) / 3;
+
+    if (a > b && a > c) {
+        mayor = a;
+    }
+    else if (b > a && b > c) {
+        mayor = b;
+    }
+    else (c > a && c > b); {
+        mayor = c;
+    }
+
+
+    if (a < b && a < c) {
+        menor = a;
+    }
+    else if (b < a && b < c) {
+        menor = b;
+    }
+    else (c < a && c < b); {
+        menor = c;
+    }
+
+    cout << "La suma de " << a << " + " << b << " + " << c << " es: " << suma << endl;
+    cout << "El promedio de " << a << ", " << b << " y " << c << " es: " << promedio << endl;
+    cout << "El producto de " << a << " x " << b << " x " << c << " es: " << producto << endl;
+    cout << "El mayor de " << a << ", " << b << " y " << c << " es: " << mayor << endl;
+    cout << "El menor de " << a << ", " << b << " y " << c << " es: " << menor << endl;
+}
+
